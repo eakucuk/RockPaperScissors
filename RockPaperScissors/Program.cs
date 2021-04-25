1bd9b4a445ebdca1d2300f0010c7f8854d17eb02
eakucuk <emreanil1903@hotmail.com>
2021-04-25 17:01:50 +0300

    ilk commit

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int win1 = 0, win2 = 0;
+            Random random = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int player2 = random.Next(0, 3);
+                if (player2 == 0)
+                {
+                    win2++;
+                }
+                else if(player2 == 2)
+                {
+                    win1++;
+                }
+            }
+            Console.WriteLine("Player A wins "+win1+ " of 100 games");
+            Console.WriteLine("Player B wins " + win2 + " of 100 games");
+            Console.WriteLine("Tie: " + (100-win1-win2) + " of 100 games");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
